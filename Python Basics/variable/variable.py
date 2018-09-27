827954f4daa725ef059b71fb30a1fad0ec75dabf
ugur.adalar <adalarugur@gmail.com>
2018-09-27 19:25:21 +0300

    variable.py added
    variable.py added

---
+++ Python Basics/variable/variable.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# %%
+#variable
+var1 = 10
+var2 = 15
+
+day = "monday"
+
+x = 12.0
+
+var5 = 10
+var7 = 19
+
+# %%
+#string
+s = "this day monday"
+
+variable_type = type(s)
+print(s)
+
+deg1 = "ank"
+deg2 = "iz"
+deg3 = deg1+deg2 #deg birleştirme
+
+degA = "nasil bir degisken bu"
+uzun =  len(degA)
+
+# %%
+#numbers
+
+#integer 
+int_try = -50
+
+#double
+
+float_try = -40.7
+
+# %% built infunction
+
+str1 = "try"
+
+float1 = 10.6
+
+str2 = "1005"
+
+# %% user defined functions
+
+#object
